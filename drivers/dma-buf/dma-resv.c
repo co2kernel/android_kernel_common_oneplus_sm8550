308ddf9d1a32a31e6842ea393172d9e32083e596
Christian König <christian.koenig@amd.com>
2026-07-13 12:34:28 +0800

    dma-buf: fix timeout handling in dma_resv_wait_timeout v2
    With a zero timeout, dma_resv_wait_timeout() must poll fences and
    return 1 if they are already signaled, rather than wait for one jiffy.
    
    [ Upstream commit 2b95a7db6e0f75587bffddbb490399cbb87e4985 ]
    
    (cherry picked from commit 09064e6d659729d01742744efecd9131cdd93c24)

--- drivers/dma-buf/dma-resv.c
+++ drivers/dma-buf/dma-resv.c
@@ -571,10 +571,14 @@ retry:
 			goto retry;
 		}
 
-		ret = dma_fence_wait_timeout(fence, intr, ret);
+		ret = dma_fence_wait_timeout(fence, intr, timeout);
 		dma_fence_put(fence);
-		if (ret > 0 && wait_all && (i + 1 < shared_count))
+		if (ret > 0 && wait_all && (i + 1 < shared_count)) {
+			/* Even for zero timeout the return value is 1 */
+			if (timeout)
+				timeout = ret;
 			goto retry;
+		}
 	}
 	return ret;
 
